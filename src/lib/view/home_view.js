577d8c55db41c8b5618b5096dde3dc19ec0e605f
Lesliee97 <eilsel1718rv@gmail.com>
2019-11-13 08:12:10 -0500

    puzzle

--- src/lib/view/home_view.js
+++ src/lib/view/home_view.js
@@ -93,14 +93,18 @@ console.log(months[d.getMonth()]);
           <iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLima&amp;src=Zm5ycmJyMnBqODg1OGtwN3RkazJrdm1ib29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23336699&amp;showTz=0&amp;showCalendars=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=1" style="border-width:0" width="400" height="400" frameborder="0" scrolling="no"></iframe>
           </div>
           <div class="div-acceso-directo">
-      
+          
               <div class="cuadrado-ad">
+              <img src="../src/imgs/puzzle-piece-plugin.svg">
                 <p>Portal de Rendiciones</p>
                 </div>
                 <div class="cuadrado-ad">
+                <img src="../src/imgs/puzzle-piece-plugin.svg">
                   <p>Formatos</p>
                 </div>
+  
                 <div class="cuadrado-ad">
+                <img src="../src/imgs/puzzle-piece-plugin.svg">
                 <p>Rist</p>
                 </div>
                 </div>
